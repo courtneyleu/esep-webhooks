93c333e83e668d5c83db4ab53e9ede2fb7ed2524
courtneyleu <80422527+courtneyleu@users.noreply.github.com>
2023-11-09 01:17:59 -0500

    Update Function.cs

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -25,9 +25,9 @@ using Amazon.Lambda.Core;
 using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be 
-converted into a .NET class.
-[assembly: 
-LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+//converted into a .NET class.
+//[assembly: 
+//LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace EsepWebhook;
 
